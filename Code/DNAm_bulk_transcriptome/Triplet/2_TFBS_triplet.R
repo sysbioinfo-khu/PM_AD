878881d39026b8aabb3fb31a968d56d140988ebc
sysbioinfo-khu <43240630+sysbioinfo-khu@users.noreply.github.com>
2025-02-04 16:24:35 +0900

    Add files via upload

---
+++ Code/DNAm_bulk_transcriptome/Triplet/2_TFBS_triplet.R
@@ -0,0 +1,102 @@
+# Set working directory
+setwd("/home/yang/PM_AD/Submission/Table/Supplementary_table3")
+
+# Load necessary libraries
+library(BSgenome.Hsapiens.UCSC.hg38)
+library(Biostrings)
+library(JASPAR2022)
+library(TFBSTools)
+library(rtracklayer)
+
+# Load data (Triplet result files generated from the 1_Triplet_analysis.R)
+promoter_data <- read.csv('Promoter.txt')
+distal_data <- read.csv('distal.txt')
+
+
+data = distal_data
+
+# Options for JASPAR
+opts <- list()
+opts[["species"]] <- 9606  # Human species
+opts[["all_versions"]] <- TRUE
+pfm_set <- getMatrixSet(JASPAR2022::JASPAR2022, opts)
+
+# Initialize lists to store p-values and GFF3 information for each row
+pvalues_list <- vector("list", nrow(data))
+gff3_list <- vector("list", nrow(data))
+
+# Loop through each row of the data
+for (i in 1:nrow(data)) {
+  # Extract TF name and regionID
+  tf_name <- data$TF_symbol[i]
+  regionID <- data$regionID[i]
+  
+  # Extract chromosome and position from regionID column(e.g., chr1:161217049-161217050)
+  region_parts <- strsplit(regionID, ":")[[1]]
+  chr <- region_parts[1]
+  positions <- strsplit(region_parts[2], "-")[[1]]
+  start_pos <- as.numeric(positions[1])
+  end_pos <- as.numeric(positions[2])
+  
+  # Find matching TF matrix
+  matched_matrices <- pfm_set[sapply(pfm_set, function(m) m@name == tf_name)]
+  
+  # If no matching TF is found, store NA for p-value and GFF3 information
+  if(length(matched_matrices) == 0) {
+    pvalues_list[[i]] <- NA
+    gff3_list[[i]] <- NA
+    next
+  }
+  
+  # Retrieve the TF matrix and convert it to a PWM (Position Weight Matrix)
+  matrix_id <- matched_matrices[[1]]@ID
+  pfm <- TFBSTools::getMatrixSet(JASPAR2022::JASPAR2022, opts = list(ID = c(matrix_id)))
+  pwm <- toPWM(pfm)
+  
+  # Extract the sequence for the specified region (+- 12 bp from regulatory CpG)
+  subject <- getSeq(BSgenome.Hsapiens.UCSC.hg38, chr, start_pos - 12, end_pos + 12)
+  
+  # Search for TF binding sites in the region
+  siteset <- searchSeq(pwm, subject, seqname="seq1", min.score="50%", strand="*")
+  
+  # Convert the binding sites to GFF3 format
+  df <- writeGFF3(siteset)
+  df$pval <- pvalues(siteset, type="sampling")[[1]]
+  
+  # Filter the GFF3 data to find regions that include positions 12-13
+  filtered_df <- df[sapply(1:nrow(df), function(i) {
+    start <- df$start[i]
+    end <- df$end[i]
+    
+    # Check if the region includes positions 12 and 13 (regulatory CpG)
+    (start <= 13 && end >= 12)
+  }), ]
+  
+  # Calculate the minimum p-value if available
+  if (length(filtered_df$pval) > 0) {
+    min_pvalue <- min(unlist(filtered_df$pval), na.rm = TRUE)  # Extract the smallest p-value
+    min_index <- which.min(unlist(filtered_df$pval))  # Find the index of the smallest p-value
+  } else {
+    min_pvalue <- NA
+    min_index <- NA
+  }
+  
+  # Store the p-value and GFF3 data in the lists
+  pvalues_list[[i]] <- min_pvalue
+  
+  # Extract GFF3 information based on the index of the smallest p-value
+  if (!is.na(min_index)) {
+    gff3_data <- filtered_df[min_index, 9]
+    gff3_list[[i]] <- gff3_data
+  } else {
+    gff3_list[[i]] <- NA
+  }
+}
+
+# Add the p-value and GFF3 information to the original data
+data$TF_binding_pvalue <- unlist(pvalues_list)
+data$GFF3_info <- unlist(gff3_list)
+
+# Save the results to CSV files
+# write.csv(data, 'Triplet_results_TFBS.txt')
+
